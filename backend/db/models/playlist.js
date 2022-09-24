b146872fb0d48027cd67395fdebcbb6500a4d469
CavinTimothy <cavintimotht2014@gmail.com>
2022-09-24 11:36:41 -0700

    Playlist model modified

--- backend/db/models/playlist.js
+++ backend/db/models/playlist.js
@@ -14,7 +14,8 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'userId'
       });
       Playlist.belongsToMany(models.Song, {
-        through: models.PlaylistSong
+        through: models.PlaylistSong,
+        foreignKey: 'playlistId'
       });
     }
   }
